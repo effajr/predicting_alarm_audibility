6245a27981b120031614813aafc038a60109d623
effajr <104824438+effajr@users.noreply.github.com>
2024-05-30 17:45:12 +0200

    Delete scheme directory

--- scheme/__init__.py
+++
@@ -1,6 +0,0 @@
-from ..scheme.models.load import load_weights
-from ..scheme.models.mymodels import CNN
-from ..scheme.data_generator import *
-from ..scheme.usemodel import *
-from ..scheme.process_annotators import *
-from ..scheme.human_baseline import human_performance
